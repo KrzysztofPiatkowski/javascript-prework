055b35e5a48aeb3997f60ec43a3c71ce6553e5f3
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-01-05 10:59:02 +0100

    Remove comments and change arguments name of function

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ function playGame(playerInput) {
 
   clearMessages();
 
-  function getMoveName(argFun) {
-    if (argFun == '1'){
+  function getMoveName(moveNumber) {
+    if (moveNumber == '1'){
     return 'kamień';
-  } else if (argFun == '2') {
+  } else if (moveNumber == '2') {
     return 'papier';
-  } else if (argFun == '3'){
+  } else if (moveNumber == '3'){
     return 'nożyce';
   } else {
     return 'nieznany ruch';
@@ -20,31 +20,11 @@ function playGame(playerInput) {
 
   let computerMove = getMoveName(randomNumber);
 
-  /* if(randomNumber == 1){
-    computerMove = 'kamień';
-  } else if (randomNumber == 2) {
-      computerMove = 'papier';
-  } else if (randomNumber == 3) {
-      computerMove = 'nożyce';
-  } */
-
   printMessage('Mój ruch to: ' + computerMove);
-
-
-
-  // let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
   console.log('Gracz wpisał: ' + playerInput);
 
   let playerMove = getMoveName(playerInput);
-
-  /* if(playerInput == '1'){
-    playerMove = 'kamień';
-  } else if (playerInput == '2') {
-    playerMove = 'papier';
-  } else if (playerInput == '3'){
-    playerMove = 'nożyce';
-  } */
 
   printMessage('Twój ruch to: ' + playerMove);
 
@@ -61,48 +41,18 @@ function playGame(playerInput) {
   }
 
   displayResult(computerMove, playerMove);
-
-
-  /* if (computerMove == playerMove) {
-    printMessage('Mamy remis!');
-  } else if ((computerMove == 'kamień' && playerMove == 'papier') || (computerMove == 'papier' && playerMove == 'nożyce') || (computerMove == 'nożyce' && playerMove == 'kamień')) {
-    printMessage('Ty wygrywasz!');
-  } else if ((computerMove == 'papier' && playerMove == 'kamień') || (computerMove == 'nożyce' && playerMove == 'papier') || (computerMove == 'kamień' && playerMove == 'nożyce')) {
-    printMessage('Ja wygrywam!');
-  } else {
-    printMessage('Wykonałeś ' + playerMove + '! Wpisuj tylko liczby: 1, 2 lub 3');
-  } */
 }
-
-/* document.getElementById('play-papier').addEventListener('click', function(){
-  printMessage('Guzik papier kliknięty');
-});
-
-document.getElementById('play-rock').addEventListener('click', function(){
-  printMessage('Guzik kamień kliknięty');
-});
-
-
-
-document.getElementById('play-scissors').addEventListener('click', function(){
-  printMessage('Guzik nożyce kliknięty');
-});  */
-
-/* function buttonClicked(playerChoice) {
-  playGame(playerChoice);
-} */
 
 let rock = document.getElementById('play-rock');
 let paper = document.getElementById('play-paper');
 let scissors = document.getElementById('play-scissors');
 
 rock.addEventListener('click', function () {
-  playGame(1); // 1 = kamień
+  playGame(1);
 });
 paper.addEventListener('click', function () {
-  playGame(2); // 2 = papier
+  playGame(2);
 });
 scissors.addEventListener('click', function () {
-  playGame(3); // 3 = nożyce
+  playGame(3);
 });
-
